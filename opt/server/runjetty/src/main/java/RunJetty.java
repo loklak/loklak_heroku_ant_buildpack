01e1876e56d96f432916620ccffb4ff395a55b1b
Jesper Joergensen <jjoergensen@salesforce.com>
2011-06-07 11:22:41 -0700

    Quick implementation of parsing DATABASE_URL

--- opt/server/runjetty/src/main/java/RunJetty.java
+++ opt/server/runjetty/src/main/java/RunJetty.java
@@ -1,3 +1,5 @@
+import java.net.URL;
+
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
@@ -8,7 +10,14 @@ public class RunJetty
     public static void main(String[] args) throws Exception
     {
         Server server = new Server();
-
+        String dburl = System.getenv("DATABASE_URL");
+        if(dburl!=null) {
+        	String[] s = dburl.split("://");
+        	String[] s2 = s[1].split("@");
+        	String[] s3 = s2[0].split(":");
+        	System.setProperty("database.url","jdbc:postgresql://"+s2[1]+"?user="+s3[0]+"&password="+s3[1]);
+        	System.out.println("Setting system property database.url = "+System.getProperty("database.url"));
+        }
         Connector connector = new SelectChannelConnector();
         connector.setPort(Integer.getInteger("jetty.port",8080).intValue());
         server.setConnectors(new Connector[] { connector });
