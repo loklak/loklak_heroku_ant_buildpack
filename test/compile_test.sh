8dec8fb5e4d71b341a78bb70b1e1457fa5cb59e7
Ryan Brainard <brainard@heroku.com>
2012-01-04 11:51:17 -0800

    Add additional assertions to testCompile and add testCompileFailed

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,9 +2,9 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-testCompile()
+createPom()
 {
-  cat > ${BUILD_DIR}/pom.xml <<EOF
+    cat > ${BUILD_DIR}/pom.xml <<EOF
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
   <modelVersion>4.0.0</modelVersion>
@@ -16,26 +16,39 @@ testCompile()
   </properties> 
 </project>
 EOF
+}
+
+testCompile()
+{
+  createPom
+
+  # Create an old settings to make sure it gets replaced with correct one
+  mkdir -p ${CACHE_DIR}/.m2
+  echo "OLD SETTINGS" > ${CACHE_DIR}/.m2/settings.xml
 
   capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
   assertEquals 0 "${rtrn}"
-  assertContains "Installing Maven 3.0.3" "`cat ${STD_OUT}`"
-  assertContains "BUILD SUCCESS" "`cat ${STD_OUT}`"
   assertEquals "" "`cat ${STD_ERR}`"
 
-  assertTrue "settings.xml should be downloaded" "[ -f ${CACHE_DIR}/.m2/settings.xml ]"
+  assertContains "Installing Maven 3.0.3" "`cat ${STD_OUT}`"
+  assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
+  
+  assertContains "Installing settings.xml" "`cat ${STD_OUT}`"
+  assertTrue "settings.xml should exist and have content" "[ -s ${CACHE_DIR}/.m2/settings.xml ]"
+  assertNotContains "OLD SETTINGS" "`cat ${CACHE_DIR}/.m2/settings.xml`" # old settings was deleted
 
-  # cache dir is created
-  # maven is downloaded
-  # ./maven/bin/mvn is executable
-  # settings.xml is downloaded
-  # mvn opts
-  # build cmd
+  assertContains "executing $CACHE_DIR/.maven/bin/mvn -B -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository -s $CACHE_DIR/.m2/settings.xml -DskipTests=true clean install" "`cat ${STD_OUT}`"  
+  assertContains "BUILD SUCCESS" "`cat ${STD_OUT}`"
 }
 
-_testCompileFailed()
+testCompileFailed()
 {
- fail
+  # Don't create POM to fail build
+  
+  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+  assertEquals 1 "${rtrn}"
+  assertContains "Failed to build app with Maven" "`cat ${STD_OUT}`" # Should this be going to STD_ERR??
+  assertEquals "" "`cat ${STD_ERR}`"
 }
 
 _testCompileKeepM2Cache()
