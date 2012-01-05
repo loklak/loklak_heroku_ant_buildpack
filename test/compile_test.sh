850046885457764d15b9d3a3e5e504d042993019
Ryan Brainard <brainard@heroku.com>
2012-01-04 17:52:32 -0800

    add MD5 and s3pository assertions

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -4,7 +4,7 @@
 
 createPom()
 {
-    cat > ${BUILD_DIR}/pom.xml <<EOF
+  cat > ${BUILD_DIR}/pom.xml <<EOF
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
   <modelVersion>4.0.0</modelVersion>
@@ -13,26 +13,45 @@ createPom()
   <version>1.0-SNAPSHOT</version>
   <properties> 
     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding> 
-  </properties> 
+  </properties>
+  <dependencies>
+$1
+  </dependencies>
 </project>
 EOF
 }
 
+withDependency()
+{
+  cat <<EOF
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>4.0</version>
+      <type>jar</type>
+      <scope>test</scope>
+    </dependency>
+EOF
+}
+
+
 testCompile()
 {
-  createPom
+  createPom "$(withDependency)" # including dependency to force use of s3pository.heroku.com
 
   capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
   assertEquals 0 "${rtrn}"
   assertEquals "" "$(cat ${STD_ERR})"
 
   assertContains "Installing Maven 3.0.3" "$(cat ${STD_OUT})"
+  assertFileMD5 "605f8b746e16576064258afaf630a2cc"  ${CACHE_DIR}/.maven/bin/mvn
   assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
   
   assertContains "Installing settings.xml" "$(cat ${STD_OUT})"
-  assertTrue "settings.xml should exist and have content" "[ -s ${CACHE_DIR}/.m2/settings.xml ]"
+  assertFileMD5 "a5fa7b9982fc64939c0e215f935a850a" ${CACHE_DIR}/.m2/settings.xml
   
   assertContains "executing $CACHE_DIR/.maven/bin/mvn -B -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository -s $CACHE_DIR/.m2/settings.xml -DskipTests=true clean install" "$(cat ${STD_OUT})"  
+  assertContains "s3pository.heroku.com" "$(cat ${STD_OUT})"
   assertContains "BUILD SUCCESS" "$(cat ${STD_OUT})"
 }
 
